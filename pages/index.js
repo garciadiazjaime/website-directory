da29132b94e9ef458a43936eb2e916a7ca565511
jaime <garciadiazjaime@gmail.com>
2017-12-31 17:43:09 -0600

    include eslint

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import LazyLoad from 'react-lazyload';
+import React from 'react'
+import LazyLoad from 'react-lazyload'
 
 import Layout from '../components/layout/layout'
 import { getPlaces } from '../services/places'
@@ -9,18 +10,18 @@ const renderPlace = (places) => {
       {
         place.google.photos.length ? 
           <LazyLoad height={200} once>
-            <img src={place.google.photos[0]} title={place.google.name} />
+            <img src={place.google.photos[0]} alt={place.google.name} />
           </LazyLoad> : null
       }
       <div className="content">
         <h2>{place.google.name}</h2>
         {
           place.google.international_phone_number ?
-          <p><a href={`tel:${place.google.international_phone_number}`}>{place.google.international_phone_number}</a></p> : null
+            <p><a href={`tel:${place.google.international_phone_number}`}>{place.google.international_phone_number}</a></p> : null
         }
         {
           place.google.url ?
-          <p><a href={place.google.url} target="_blank">ver en google maps</a></p> : null
+            <p><a href={place.google.url} target="_blank">ver en google maps</a></p> : null
         }
       </div>
       <style jsx global>{`
--- services/places.js
+++ services/places.js
@@ -1,9 +1,8 @@
 import fetch from 'isomorphic-unfetch'
 
-// export const proxyUrl = process && process.env && process.env.PROXY_URL ? process.env.PROXY_URL : window.proxyUrl
-const proxyUrl = 'http://0.0.0.0:3030/'
+export const apiUrl = process && process.env && process.env.API_URL ? process.env.API_URL : null
 
 export async function getPlaces() {
-  const res = await fetch(`${proxyUrl}places`)
+  const res = await fetch(`${apiUrl}places`)
   return await res.json()
 }
