667ef84f761b12a8c825a8a0a10e9bbe40dc2e5c
jaime <garciadiazjaime@gmail.com>
2017-12-31 19:02:37 -0600

    include ganalytcs

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,36 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Head from 'next/head'
+import ReactGA from 'react-ga';
 
 import MainHeader from './mainHeader'
 
-const Layout = ({ children }) => (
-  <div>
-    <Head>
-      <title>Restaurantes, Bares y Cafés en Playas de Tijuana</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="description" content="Restaurantes, Bares y Cafés en Playas de Tijuana" />
-      <link rel="icon" href="/static/images/favicon.ico" />
-    </Head>
+class Layout extends Component {
 
-    <MainHeader />
+  componentDidMount() {
+    ReactGA.initialize('UA-75576478-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
 
-    {children}
+  render() {
+    return (
+      <div>
+        <Head>
+          <link rel="icon" href="/static/images/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
 
-    <style jsx global>{`
-      body {
-        background-color: #f5f6f6;
-        color: #000;
-        font-size: 26px;
-        margin: 0;
-        font-family: Helvetica;
-      }
+          <title>Restaurantes, Bares y Cafés en Playas de Tijuana</title>
+          <meta name="description" content="Restaurantes, Bares y Cafés en Playas de Tijuana" />
+        </Head>
 
-      a {
-        text-decoration: none;
-      }
-    `}
-    </style>
-  </div>
-)
+        <MainHeader />
+
+        {this.props.children}
+
+        <style jsx global>{`
+          body {
+            background-color: #f5f6f6;
+            color: #000;
+            font-size: 26px;
+            margin: 0;
+            font-family: Helvetica;
+          }
+
+          a {
+            text-decoration: none;
+          }
+        `}
+        </style>
+      </div>
+    )
+  }
+}
 
 export default Layout
