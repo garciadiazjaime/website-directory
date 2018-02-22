45e1a192a3fecdc2a1326bbf51c962feef24ef7c
jaime <garciadiazjaime@gmail.com>
2018-02-21 21:56:01 -0600

    display img, title and ctas

--- components/place.js
+++ components/place.js
@@ -1,66 +1,45 @@
 import React from 'react'
+import { Card, CardActions, CardMedia, CardTitle } from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
 
 import LazyLoad from 'react-lazyload'
 
+const titleStyle = {
+  padding: '0 16px 0'
+}
+
+const ctaStyle = {
+  padding: '0 0 16px'
+}
+
+const imageStyle = {
+  width: '100%',
+  height: 'auto'
+}
+
+const toTitle = title => {
+  const bits = title.toLowerCase().split(' ')
+  return bits.map(bit => `${bit.charAt(0).toUpperCase()}${bit.substr(1)}`).join(' ')
+}
+
 const Element = ({ place }) => (
-  <div key={place.placeId} className="place">
-    <div className="image-contain">
-      <div className="image-wrapper">
-        {
-          place.google.photos.length ?
-            <LazyLoad height={200} once>
-              <img src={place.google.photos[0]} alt={place.google.name} />
-            </LazyLoad> : null
-        }
-      </div>
-    </div>
-    <div className="info-contain">
-      <h2>{place.google.name}</h2>
-    </div>
-    <style jsx global>{`
-      .place {
-        margin: 20px 0 0
-        background-color: #FFF
-        display: flex
-        padding-bottom: 12px
-        align-items: center
-      }
-
-      .place .image-contain {
-
-      }
-      .place .image-wrapper {
-        width: 150px
-        height: 100px
-        overflow: hidden
-      }
-      .place img {
-        width: 180px
-        height: auto
-      }
-
-      .place .info-contain {
-        padding: 0 6px
-        overflow: hidden
-        height: 100%
-      }
-      .place h2 {
-        margin: 0
-        font-size: 2.5rem
-        overflow-wrap: break-word
-        font-weight: normal
-        opacity: 0.85
-      }
-      .place p {
-        margin: 0
-      }
-      .place a {
-        color: #000
-        opacity: 0.7
-      }
-    `}
-    </style>
-  </div>
+  <Card>
+    <CardMedia>
+      <LazyLoad height={300} once>
+        <img
+          src={place.google.photos.length ? place.google.photos[0] : `/static/images/categories/${place.category}.jpg`}
+          alt={place.google.name}
+          style={imageStyle}
+        />
+      </LazyLoad>
+    </CardMedia>
+    <CardTitle title={toTitle(place.google.name)} style={titleStyle} />
+    <CardActions style={ctaStyle}>
+      <FlatButton label="Menú" />
+      <FlatButton label="Mapa" />
+      <FlatButton label="Tel" />
+    </CardActions>
+  </Card>
 )
 
 export default Element
